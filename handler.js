2005e0a6e228ce369213b82dde6c7f06b23f5e73
katspaugh <ivan@mycs.com>
2017-06-24 11:40:59 +0200

    added voices

--- handler.js
+++ handler.js
@@ -13,22 +13,35 @@ function response(view) {
 
 function errorResponse(err) {
   console.log(err);
-  return { statusCode: 500 };
+  return {
+    statusCode: 500,
+    body: {
+      error: err.message
+    }
+  }
 }
 
 module.exports.speak = (event, context, callback) => {
+  const defaultLanguage = 'en';
+  const params = JSON.parse(event.body);
+
   const speak = spawn(path.resolve('./speak'), [
     '--path=' + path.resolve('.'),
     '-q',
     '--ipa',
-    '--stdin'
+    '--stdin',
+    `-v${ params.language.toLowerCase() || defaultLanguage }`
   ]);
 
   speak.stdin.setEncoding('utf-8');
-  speak.stdout.pipe(event.body.trim());
+  speak.stdin.write(params.text);
+  speak.stdin.end();
 
   speak.stdout.on('data', (data) => {
-    callback(null, response(data.toString('utf-8')));
+    callback(null, response({
+      language: params.language,
+      text: data.toString('utf-8').trim()
+    }))
   });
 
   speak.stderr.on('data', (data) => {
